ea061ed02de25094a0d5b4afff110c664b03e4e1
Alex Chien <alex.chien@autodesk.com>
2017-04-25 23:20:38 -0700

    Add more participants and assets when bootstrapping network

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ class App {
                 LOG.info('ProjectRegistry:<init>', 'businessNetworkDefinition obtained', this.businessNetworkDefinition.getIdentifier());
             })
             // and catch any exceptions that are triggered
-            .catch(function (error) {
+            .catch((error) => {
                 throw error;
             });
     }
@@ -30,16 +30,16 @@ class App {
     bootstrap() {
         LOG.info('ProjectRegistry:_bootstrap', 'getting asset registry for "org.acme.project.bidding.Project"');
         let factory = this.businessNetworkDefinition.getFactory();;
-        let owner;
+        let owner, owner2;
         return this.bizNetworkConnection.getAssetRegistry('org.acme.project.bidding.Project')
             .then((projectsRegistry) => {
                 LOG.info('ProjectRegistry:_bootstrap', 'getting factory and adding assets');
                 
-                LOG.info('ProjectRegistry:_bootstrap', 'Creating an architect');
+                LOG.info('ProjectRegistry:_bootstrap', 'Creating an architect 1');
                 owner = factory.newInstance('org.acme.project.bidding', 'Architect', 'email:a001');
-                owner.firstName = 'Alex';
-                owner.lastName = 'Best';
-                owner.balance = 10000;
+                owner.firstName = 'Anthony';
+                owner.lastName = 'Atkison';
+                owner.balance = 50000;
 
                 // Create a new relationship for the owner
                 let ownerRelation = factory.newRelationship('org.acme.project.bidding', 'Architect', 'email:a001');
@@ -48,14 +48,27 @@ class App {
                 let project1 = factory.newInstance('org.acme.project.bidding', 'Project', 'id:p001');
                 project1.owner = ownerRelation;
 
+                LOG.info('ProjectRegistry:_bootstrap', 'Creating an architect 2');
+                owner2 = factory.newInstance('org.acme.project.bidding', 'Architect', 'email:a002');
+                owner2.firstName = 'James';
+                owner2.lastName = 'Prestwood';
+                owner2.balance = 50000;
+
+                // Create a new relationship for the owner
+                let ownerRelation2 = factory.newRelationship('org.acme.project.bidding', 'Architect', 'email:a002');
+
+                LOG.info('ProjectRegistry:_bootstrap', 'Creating a project #2');
+                let project2 = factory.newInstance('org.acme.project.bidding', 'Project', 'id:p002');
+                project2.owner = ownerRelation2;
+
                 LOG.info('ProjectRegistry:_bootstrap', 'Adding these to the project registry');
-                return projectsRegistry.addAll([project1]);
+                return projectsRegistry.addAll([project1, project2]);
             })
             .then(() => {
                 return this.bizNetworkConnection.getParticipantRegistry('org.acme.project.bidding.Architect');
             })
             .then((architectRegistry) => {
-                return architectRegistry.add(owner);
+                return architectRegistry.addAll([owner, owner2]);
             })
             .then(() => {
                 return this.bizNetworkConnection.getParticipantRegistry('org.acme.project.bidding.Contractor');
@@ -63,14 +76,14 @@ class App {
             .then((contractorRegistry) => {
                 LOG.info('ProjectRegistry:_bootstrap', 'Creating a contractor 1');
                 let contractor1 = factory.newInstance('org.acme.project.bidding', 'Contractor', 'email:c001');
-                contractor1.firstName = 'Chase';
-                contractor1.lastName = 'Dash';
+                contractor1.firstName = 'Don';
+                contractor1.lastName = 'Rushin';
                 contractor1.balance = 0;
 
                 LOG.info('ProjectRegistry:_bootstrap', 'Creating a contractor 2');
                 let contractor2 = factory.newInstance('org.acme.project.bidding', 'Contractor', 'email:c002');
-                contractor2.firstName = 'Cyrus';
-                contractor2.lastName = 'Daniel';
+                contractor2.firstName = 'Nathan';
+                contractor2.lastName = 'Rolle';
                 contractor2.balance = 0;
 
                 return contractorRegistry.addAll([contractor1, contractor2]);
@@ -82,18 +95,33 @@ class App {
                 LOG.info('ProjectRegistry:_bootstrap', 'Creating a project list 1');
                 let listing1 = factory.newInstance('org.acme.project.bidding', 'ProjectListing', 'listing:l001');
                 listing1.estimateCost = 5000;
-                listing1.description = "SF House Renovation";
+                listing1.description = "Landmark Office Renovation";
                 listing1.state = "FOR_BIDDING";
                 listing1.status = "GC_BIDDING";
-                listing1.sector = "PRIVATE";
+                listing1.sector = "PUBLIC_STATE";
                 listing1.buildingUse = "OFFICE";
                 listing1.constructionType = "RENOVATION";
-                let projectRelation = factory.newRelationship('org.acme.project.bidding', 'Project', 'id:p001');
-                listing1.project = projectRelation;
+                let projectRelation2 = factory.newRelationship('org.acme.project.bidding', 'Project', 'id:p001');
+                listing1.project = projectRelation2;
 
-                return projectListRegistry.addAll([listing1]);
+                LOG.info('ProjectRegistry:_bootstrap', 'Creating a project list 2');
+                let listing2 = factory.newInstance('org.acme.project.bidding', 'ProjectListing', 'listing:l002');
+                listing2.estimateCost = 10000;
+                listing2.description = "SF Hotel Renovation";
+                listing2.state = "FOR_BIDDING";
+                listing2.status = "GC_BIDDING";
+                listing2.sector = "PRIVATE";
+                listing2.buildingUse = "HOTEL";
+                listing2.constructionType = "RENOVATION";
+                let projectRelation = factory.newRelationship('org.acme.project.bidding', 'Project', 'id:p002');
+                listing2.project = projectRelation;
+
+                return projectListRegistry.addAll([listing1, listing2]);
             })
-            .catch(function (error) {
+            .then(() => {
+                LOG.info('ProjectRegistry:_bootstrap', 'Done');
+            })
+            .catch((error) => {
                 console.log(error);
                 LOG.error('ProjectRegistry:_bootstrap', error);
                 throw error;
